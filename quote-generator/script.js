da3d1e746190a7a6d050f450deefe085158f43ce
r4vaa <patildheeraj56@gmail.com>
2024-07-15 11:39:32 +0530

    added loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -5,13 +5,32 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// // Show New quote
 
+// Show Loading
+
+function loading() {
+   loader.hidden = false;
+   quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+   quoteContainer.hidden = false;
+   loader.hidden = true;
+}
+
+
+// // Show New quote
 function newQuote() {
-   //    // Pick a random quote from apiQuotes array
+
+   loading();
+
+   //  Pick a random quote from apiQuotes array
 
    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
    // Check if Author field is blank and replace it with blank
@@ -28,8 +47,10 @@ function newQuote() {
    } else {
       quoteText.classList.remove('long-quote');
    }
-   quoteText.textContent = quote.text;
 
+   // Set Quote, Hide Loader
+   quoteText.textContent = quote.text;
+   complete();
 
 }
 
@@ -39,6 +60,7 @@ function newQuote() {
 
 
 async function getQuotes() {
+   loading();
    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
    try {
@@ -67,5 +89,6 @@ twitterBtn.addEventListener('click', tweetQuote);
 // on load
 
 getQuotes();
+
 
 
